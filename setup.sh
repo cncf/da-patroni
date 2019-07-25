2abcb214eb7da859c03111fdeeb661d8dae96b12
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-25 11:37:57 +0000

    Update command
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ change_namespace.sh $1 devstats
 ##"${1}h.sh" install --dry-run --debug --generate-name ./da-patroni --set skipSecret=1,skipBackupsPV=1,skipBackups=1,skipNamespace=1
 "${1}h.sh" install patroni-secret ./da-patroni --set "skipBackupsPV=1,skipBackups=1,skipPostgres=1,skipNamespace=1"
 "${1}h.sh" install patroni ./da-patroni --set "skipSecret=1,skipBackupsPV=1,skipBackups=1,skipNamespace=1"
-"${1}h.sh" install patroni-backup ./da-patroni --set "skipSecret=1,skipNamespace=1,skipPostgres=1,backupsCommand='/bin/sleep',backupsCommandArgs={36000s}"
+"${1}h.sh" install patroni-backup ./da-patroni --set "skipSecret=1,skipNamespace=1,skipPostgres=1"
 change_namespace.sh $1 default
