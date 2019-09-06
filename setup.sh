01139a116dc3c957d89f85862b0eff5483488ec7
lukaszgryglicki <lukaszgryglicki@o2.pl>
2019-09-06 06:34:54 +0000

    Add devstats namespace creation step
    Signed-off-by: lukaszgryglicki <lukaszgryglicki@o2.pl>

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ then
   echo "$0: you need to specify env: test, dev, stg, prod"
   exit 1
 fi
+"${1}h.sh" install devstats-namespace ./da-patroni --set "deployEnv=$1,skipSecret=1,skipBackupsPV=1,skipBackups=1,skipPostgres=1"
 change_namespace.sh $1 devstats
 ##"${1}h.sh" install --dry-run --debug --generate-name ./da-patroni --set skipSecret=1,skipBackupsPV=1,skipBackups=1,skipNamespace=1
 "${1}h.sh" install patroni-secret ./da-patroni --set "deployEnv=$1,skipBackupsPV=1,skipBackups=1,skipPostgres=1,skipNamespace=1"
